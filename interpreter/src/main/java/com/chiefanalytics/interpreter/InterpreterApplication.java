b01055b0aaeae23ab806154c498fd876bc25e8b7
Zach Yam <thepredestrian@gmail.com>
2017-10-01 20:23:14 -0400

    interpreter first commit

---
+++ interpreter/src/main/java/com/chiefanalytics/interpreter/InterpreterApplication.java
@@ -0,0 +1,12 @@
+package com.chiefanalytics.interpreter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterpreterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterpreterApplication.class, args);
+	}
+}
