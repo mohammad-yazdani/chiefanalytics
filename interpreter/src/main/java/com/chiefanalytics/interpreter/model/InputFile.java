6f3ac3161ffb95608ae177561821c48f39060382
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2017-10-16 21:16:38 -0400

    init classes

--- interpreter/src/main/java/com/chiefanalytics/interpreter/model/InputFile.java
+++ interpreter/src/main/java/com/chiefanalytics/interpreter/model/InputFile.java
@@ -1,4 +1,19 @@
 package com.chiefanalytics.interpreter.model;
 
-public class InputFile {
+import java.io.File;
+import java.io.InputStream;
+
+public abstract class InputFile {
+
+    // TODO : Implement super ctor
+
+    private File originx;
+
+    // TODO : Change later
+    private Object data;
+
+    private Metadata metadata;
+
+    // TODO : Implement
+    public InputStream stringify() {return null;}
 }
---
+++ interpreter/src/main/java/com/chiefanalytics/interpreter/model/Metadata.java
@@ -0,0 +1,20 @@
+package com.chiefanalytics.interpreter.model;
+
+import java.util.Map;
+
+public class Metadata {
+
+    // TODO : Methods and ctor
+
+    private Map<Map<Integer, Integer>,Integer> table;
+
+    private Map<Integer, Integer> noTable;
+
+    private Map<Integer, Integer> dict;
+
+    public void addToTable(int row, int col) {}
+
+    public void addToNoTable(int row) {}
+
+    public void addToDict(int length) {}
+}
