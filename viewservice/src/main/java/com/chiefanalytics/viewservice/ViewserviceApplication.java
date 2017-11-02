d069ceb837958120bcbfc37c404b8536a9e80a1a
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2017-11-02 12:48:23 -0400

    view service added

---
+++ viewservice/src/main/java/com/chiefanalytics/viewservice/ViewserviceApplication.java
@@ -0,0 +1,12 @@
+package com.chiefanalytics.viewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewserviceApplication.class, args);
+	}
+}
