470bbb6e254cfb1b34ba2f970402f9cbfd1c309d
m5yazdan <m5yazdan@mc-3004-13.student.cs.uwaterloo.ca>
2017-12-16 17:36:00 -0500

    view service merged

---
+++ viewservice/app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Files from "./container/Files";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Files/>
+      </div>
+    );
+  }
+}
+
+export default App;
